26446e60f411a133ed79ae1b2cc44915e0ace497
liuweijie <liuweijie@ixiaowo.com>
2019-02-13 10:19:06 +0800

    去除不必要代码

--- src/main/java/io/amoe/consumer/pojo/UserVO.java
+++ src/main/java/io/amoe/consumer/pojo/UserVO.java
@@ -2,7 +2,6 @@ package io.amoe.consumer.pojo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.NumberFormat;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -17,7 +16,6 @@ public class UserVO implements Serializable {
     private String name;
     private Integer age;
     private String phone;
-    @NumberFormat(pattern="#,###,###,###.##", style = NumberFormat.Style.CURRENCY)
     private BigDecimal wallet;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime;
